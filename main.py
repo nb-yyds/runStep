b302182becc4a92eedce77a7f3ab00efe693c3e4
nb-yyds <m17133373452@163.com>
2024-12-08 16:54:12 +0800

    Update main.py

--- main.py
+++ main.py
@@ -38,6 +38,8 @@ def get_min_max_by_time(hour=None, minute=None):
     if minute is None:
         minute = time_bj.minute
     time_rate = min((hour * 60 + minute) / (22 * 60), 1)
+    print(f"当前执行时间：{hour} --- {minute} --- {time_rate}")
+    
     min_step = get_int_value_default(config, 'MIN_STEP', 21056)
     max_step = get_int_value_default(config, 'MAX_STEP', 26101)
     return int(time_rate * min_step), int(time_rate * max_step)
